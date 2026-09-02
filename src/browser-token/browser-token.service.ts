1a9f9533e6b039bf6ccf801c4f41781e7500ed05
omnixys-01 <133212199+Caleb-Script@users.noreply.github.com>
2026-09-02 09:01:24 +0200

    fix(analytics): accept UUIDv7 in id patterns

--- src/browser-token/browser-token.service.ts
+++ src/browser-token/browser-token.service.ts
@@ -218,7 +218,7 @@ function validateIssue(input: BrowserTokenIssue): void {
     (input.application !== undefined &&
       input.application !== 'checkpoint' &&
       input.application !== 'wedding') ||
-    !/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+    !/^[0-9a-f]{8}-[0-9a-f]{4}-[1-7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
       input.organizationId,
     ) ||
     !URL.canParse(input.origin) ||
--- src/processing/event-processing.service.ts
+++ src/processing/event-processing.service.ts
@@ -19,7 +19,7 @@ import {
 } from "./processing-metrics.service.js";
 
 const UUID_PATTERN =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-7][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 @Injectable()
 export class EventProcessingService {
